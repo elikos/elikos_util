87654b791d764e8ad5db5c20d0bf1dd82d4bf8f8
pierre-yves lajoie <pierre-yves.lajoie@polymtl.ca>
2017-05-15 20:42:18 -0400

    Remapper de frames tf.

--- elikos_vicon_remapping/elikos_vicon_remapping/elikos_vicon_remapping.py
+++ elikos_vicon_remapping/elikos_vicon_remapping/elikos_vicon_remapping.py
@@ -2,24 +2,33 @@
 # -*- coding: utf-8 -*-
 import rospy
 from geometry_msgs.msg import PoseStamped
+import tf
+import roslib
    
 class elikos_vicon_remapping(object):
-
-    def msg_callback(self, data):
-        self.pose_actuelle = data
-        self.pub.publish(self.pose_actuelle)
-        print 'lol'
 
     def vicon_remapping(self):
         self.pose_actuelle = PoseStamped()
 
-        in_topic = rospy.get_param('/elikos_vicon_remapping/in_topic')
-        out_topic = rospy.get_param('/elikos_vicon_remapping/out_topic')
-        self.pub = rospy.Publisher(out_topic, PoseStamped, queue_size=10)
-        rospy.Subscriber(in_topic, PoseStamped, self.msg_callback)
+        vicon_ref_frame_id = rospy.get_param('/elikos_vicon_remapping/vicon_ref_frame_id')
+        vicon_child_frame_id = rospy.get_param('/elikos_vicon_remapping/vicon_child_frame_id')
+        elikos_ref_frame_id = rospy.get_param('/elikos_vicon_remapping/elikos_ref_frame_id')
+        elikos_child_frame_id = rospy.get_param('/elikos_vicon_remapping/elikos_child_frame_id')
+
+        listener = tf.TransformListener()
+        broadcaster = tf.TransformBroadcaster()
 
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
+            try:
+                (trans,rot) = listener.lookupTransform(vicon_ref_frame_id, vicon_child_frame_id, rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+            broadcaster.sendTransform(trans,
+                        rot,
+                        rospy.Time.now(),
+                        elikos_child_frame_id,
+                        elikos_ref_frame_id)    
             rate.sleep()
 
     def __init__(self):
